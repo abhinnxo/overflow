cd8dd55eaad6e877f7d3d94741084e60deca5fe7
Abhinn Krishn <abhinnk21@gmail.com>
2023-12-02 00:26:17 +0530

    FIX: hitman image name

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,4 +1,4 @@
-import gameImage from "../assets/Hitman.jpg";
+import GameImage from "../assets/hitman.jpg";
 
 const GameCard = () => {
   return (
@@ -7,10 +7,10 @@ const GameCard = () => {
       <div className="p-4">
         <h1 className="text-3xl text-white">Hitman III</h1>
         <div className="w-[290px] flex gap-2 flex-wrap my-4">
-          <img src={gameImage} width="140px" alt="game image" />
-          <img src={gameImage} width="140px" alt="game image" />
-          <img src={gameImage} width="140px" alt="game image" />
-          <img src={gameImage} width="140px" alt="game image" />
+          <img src={GameImage} width="140px" alt="game image" />
+          <img src={GameImage} width="140px" alt="game image" />
+          <img src={GameImage} width="140px" alt="game image" />
+          <img src={GameImage} width="140px" alt="game image" />
         </div>
         <p className="text-white">tags: Action, RPG, Adventure</p>
         <p className="text-white">Rs. 1,299</p>
