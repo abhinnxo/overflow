85e68f96462cc07e9c402325ca1fbecf42e2d621
Abhinn Krishn <abhinnk21@gmail.com>
2023-12-03 09:33:56 +0530

    ADD: special ofers component

--- src/components/SpecialOffers.jsx
+++ src/components/SpecialOffers.jsx
@@ -1,23 +1,60 @@
-import React from "react";
+import { useState } from "react";
+import { FaRegBookmark } from "react-icons/fa6";
+import { FaBookmark } from "react-icons/fa6";
+import Hitman from "../assets/hitman.jpg";
 
 const SpecialOffers = () => {
+  const [wishlistState, setWishlistState] = useState(false);
+
+  const Card = () => {
+    return (
+      <div className="gamecard carousel-item w-[380px] rounded bg-furfle">
+        <div className="w-[380px]">
+          <div className="absolute text-white">
+            <span className="absolute font-semibold mt-4 ml-2.5">-99%</span>
+            <FaBookmark className="text-red-600 text-6xl" />
+          </div>
+          <img
+            src={Hitman}
+            alt="pic"
+            className="w-[620px] h-[100%]"
+            id="hero-car-game-img"
+          />
+        </div>
+        <div className="p-4 w-[380px]">
+          <h1 className="text-3xl text-white">Hitman III</h1>
+          <p className="text-white mt-2.5">Rs. 1,299</p>
+          <div className="mt-3 ml-auto flex justify-end items-center gap-4">
+            <span
+              className="text-white cursor-pointer"
+              onClick={() => setWishlistState(!wishlistState)}
+            >
+              {wishlistState ? (
+                <FaBookmark className="text-2xl" />
+              ) : (
+                <FaRegBookmark className="text-2xl" />
+              )}
+            </span>
+            <div className="text-furfle bg-white rounded-md w-fit py-2 px-4 hover:bg-furfle outline outline-1 hover:text-white cursor-pointer">
+              Add to cart
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="lg:container flex mx-auto flex-col">
       <h1 className="text-4xl font-bold text-center py-10 text-lavander">
         Special Offers
       </h1>
-      <div className="grid grid-cols-3 grid-rows-1 gap-4 w-[fit] h-[250px] mx-auto mb-24">
-        <div className="w-[250px] bg-green-600">
-          <div className="h-[200px] bg-red-600 p-2">
-            <p className="text-white text-lg">-35%</p>
-          </div>
-          <div className="h-[100px] bg-blue-500 py-2 px-4">
-            <h1 className="text-2xl text-white text-center">Hitman III</h1>
-          </div>
-        </div>
-        <div className="w-[250px] bg-green-600">b</div>
-        <div className="w-[250px] bg-green-600">c</div>
+      <div className=" w-[fit] h-[250px] grid grid-cols-3 gap-4 mx-auto mb-24">
+        <Card />
+        <Card />
+        <Card />
       </div>
+      <div className="my-32"></div>
     </div>
   );
 };
